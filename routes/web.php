26254e5c89700c7f1e08996083e0944cb87a713c
Khusanboy <119614237+khusanboytursunov@users.noreply.github.com>
2023-03-10 15:17:29 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/test', function () {
+Route::get('/salom', function () {
     return view('welcome');
 });
