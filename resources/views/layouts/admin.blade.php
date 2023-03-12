1823b274ce85d66c2cee40badaf8278305e48569
abdurahmon <118608138+isaqov1700@users.noreply.github.com>
2023-03-12 15:10:48 +0500

    admin

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -35,7 +35,7 @@
                     </li>
 
                     <li class="sidebar-item active">
-                        <a class="sidebar-link" href="index.html">
+                        <a class="sidebar-link" href="{{asset('admin')}}">
                             <i class="align-middle" data-feather="sliders"></i> <span
                                 class="align-middle">Dashboard</span>
                         </a>
