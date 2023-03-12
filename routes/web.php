12a6a25932973c9465dc89d03369362e22830c02
my-white1 <mywhite060608@gmail.com>
2023-03-12 09:28:48 +0300

    front to public
    added frontend template to public derectory

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/vfdsdfsvfd', function () {
-    return view('welcomevdzbdf');
+Route::get('/test', function () {
+    return 'test';
 });
