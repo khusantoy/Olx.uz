de2112798938174c71ba84096568b3d0cefae35f
abdurahmon <118608138+isaqov1700@users.noreply.github.com>
2023-03-18 20:33:04 +0500

    checkbox

---
+++ resources/views/auth/oferta.blade.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>OLX - Qulay joyda xaridlar</title>
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png">
+
+    <!-- Animate CSS -->
+    <link rel="stylesheet" href="assets/css/animate.min.css">
+
+    <!-- Main CSS -->
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+
+<!-- Main -->
+<main class="main">
+    <div class="container">
+        <div class="page-header breadcrumb-wrap">
+            <div class="breadcrumb">
+                <a href="index.html" rel="nofollow"><i class="fas fa-home mr-10"></i>Home</a>
+                <span></span> Terms and Conditions
+            </div>
+        </div>
+    </div>
+    <div class="page-content">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="terms-content">
+                        <div class="terms-header text-center">
+                            <h4>Terms of Use of VEBMUHANDIS.com</h4>
+                        </div>
+                        <div class="terms-text">
+                            <p>PLEASE READ THE FOLLOWING TERMS AND CONDITIONS OF USE CAREFULLY BEFORE USING THIS WEBSITE.</p>
+
+                            <p>All users of this site agree that access to and use of this site is subject to the following terms and conditions and other applicable law. If you do not agree to these terms and conditions, please do not use this site.</p>
+
+                            <h5>Disclaimer</h5>
+
+                            <p>You understand that it is your responsibility to ensure that the privacy policy you create is complete, accurate, and meets your companies specific privacy needs. VEBMUHANDISdemo.com is not liable or responsible for any privacy policies created using our services, and we give no representations or warranties, express or implied, that the privacy policies created using our service are complete, accurate or free from errors or omissions.</p>
+
+                            <p>VEBMUHANDISdemo.com is a family friendly site and we DO NOT intentionally accept or allow the following types of sites into our program: Gambling, Adult content (porn, soft porn, sites with adult ad's), Pharmacy (Cheap drugs, Viagra, male/female enhancement, etc.), Hate, Link Farms or Spam Sites. If you sell any of these products and we find out, we will cancel your membership without hesitation. We do not need to explain our decision or reasons if we reject or cancel any membership.</p>
+
+                            <h5>Refunds & Guarantees</h5>
+
+                            <p>VEBMUHANDISdemo.com offers the following guarantee. If you purchase a VEBMUHANDISdemo.com course, product or service, and for some reason you decide that you would like a refund, you have 7 days to request a refund. If you request a refund within 7 days from the date of purchase, VEBMUHANDISdemo.com will give you a full refund of your purchase price for the course, product or service. If you do not request a refund within the 7 day refund period, you forfeit this option and will not be eligible for a refund. We do not offer refunds on any additional services that you may purchase in the members area once you are a member.</p>
+
+                            <h5>Email Opt-in Policy</h5>
+
+                            <p>When using our VEBMUHANDISdemo.com Generator service you will be opted-in to receive weekly email updates, tips and suggestions we believe will help build, grow and enhance your site. You may unsubscribe at any time by clicking on the "Unsubscribe or Modify my subscription" link at the bottom of any email sent.</p>
+
+                            <h5>Copyright</h5>
+
+                            <p>The entire content included in this site, including but not limited to text, graphics or code is copyrighted as a collective work under the United States and other international copyright laws, and is the property of VEBMUHANDISdemo.com. The collective work includes works that are licensed to VEBMUHANDISdemo.com. Permission is granted to electronically copy and print hard copy portions of this site for the sole purpose of placing an order with VEBMUHANDISdemo.com or purchasing our products. Any other use, including but not limited to the reproduction, distribution, display or transmission of the content of this site is strictly prohibited, unless authorized by VEBMUHANDISdemo.com. You further agree not to change or delete any proprietary notices from materials downloaded from the site.</p>
+
+                            <h5>Trademarks</h5>
+
+                            <p>All trademarks, service marks and trade names of VEBMUHANDISdemo.com used in the site are trademarks or registered trademarks of VEBMUHANDISdemo.com.</p>
+
+                            <h5>Warranty Disclaimer</h5>
+
+                            <p>This site and the materials and products on this site are provided "as is" and without warranties of any kind, whether express or implied. To the fullest extent permissible pursuant to applicable law, VEBMUHANDISdemo.com disclaims all warranties, express or implied, including, but not limited to, implied warranties of merchantability and fitness for a particular purpose and non-infringement. VEBMUHANDISdemo.com does not represent or warrant that the functions contained in the site will be uninterrupted or error-free, that the defects will be corrected, or that this site or the server that makes the site available are free of viruses or other harmful components. VEBMUHANDISdemo.com does not make any warrantees or representations regarding the use of the materials in this site in terms of their correctness, accuracy, adequacy, usefulness, timeliness, reliability or otherwise. Some states do not permit limitations or exclusions on warranties, so the above limitations may not apply to you.</p>
+
+                            <h5>Limitation of Liability</h5>
+
+                            <p>VEBMUHANDISdemo.com shall not be liable for any special or consequential damages that result from the use of, or the inability to use, the services and products offered on this site, or the performance of the services and products.</p>
+
+                            <h5>Typographical Errors</h5>
+
+                            <p>In the event that a VEBMUHANDISdemo.com product is mistakenly listed at an incorrect price, VEBMUHANDISdemo.com reserves the right to refuse or cancel any orders placed for product listed at the incorrect price. VEBMUHANDISdemo.com reserves the right to refuse or cancel any such orders whether or not the order has been confirmed and your credit card charged. If your credit card has already been charged for the purchase and your order is cancelled, VEBMUHANDISdemo.com will issue a credit to your credit card account in the amount of the incorrect price.</p>
+
+                            <h5>Term & Termination</h5>
+
+                            <p>These terms and conditions are applicable to you upon your accessing the site and/or completing the registration or shopping process. These terms and conditions, or any part of them, may be terminated by VEBMUHANDISdemo.com without notice at any time, for any reason. The provisions relating to Copyrights, Trademark, Disclaimer, Limitation of Liability, Indemnification and Miscellaneous, shall survive any termination.</p>
+
+                            <h5>Use of Site</h5>
+
+                            <p>Harassment in any manner or form on the site, including via e-mail, chat, or by use of obscene or abusive language, is strictly forbidden. Impersonation of others, including a VEBMUHANDISdemo.com or other licensed employee, host, or representative, as well as other members or visitors on the site is prohibited. You may not upload to, distribute, or otherwise publish through the site any content which is libelous, defamatory, obscene, threatening, invasive of privacy or publicity rights, abusive, illegal, or otherwise objectionable which may constitute or encourage a criminal offense, violate the rights of any party or which may otherwise give rise to liability or violate any law. You may not upload commercial content on the site or use the site to solicit others to join or become members of any other commercial online service or other organization.</p>
+
+                            <h5>Participation Disclaimer</h5>
+
+                            <p>VEBMUHANDISdemo.com does not and cannot review all communications and materials posted to or created by users accessing the site, and are not in any manner responsible for the content of these communications and materials. You acknowledge that by providing you with the ability to view and distribute user-generated content on the site, VEBMUHANDISdemo.com is merely acting as a passive conduit for such distribution and is not undertaking any obligation or liability relating to any contents or activities on the site. However, VEBMUHANDISdemo.com reserves the right to block or remove communications or materials that it determines to be (a) abusive, defamatory, or obscene, (b) fraudulent, deceptive, or misleading, (c) in violation of a copyright, trademark or; other intellectual property right of another or (d) offensive or otherwise unacceptable to VEBMUHANDISdemo.com in its sole discretion.</p>
+
+                            <h5>Indemnification</h5>
+
+                            <p>You agree to indemnify, defend, and hold harmless VEBMUHANDISdemo.com, its officers, directors, employees, agents, licensors and suppliers (collectively the "Service Providers") from and against all losses, expenses, damages and costs, including reasonable attorneys' fees, resulting from any violation of these terms and conditions or any activity related to your account (including negligent or wrongful conduct) by you or any other person accessing the site using your Internet account.</p>
+
+                            <h5>Third-Party Links</h5>
+
+                            <p>In an attempt to provide increased value to our visitors, VEBMUHANDISdemo.com may link to sites operated by third parties. However, even if the third party is affiliated with VEBMUHANDISdemo.com, VEBMUHANDISdemo.com has no control over these linked sites, all of which have separate privacy and data collection practices, independent of VEBMUHANDISdemo.com. These linked sites are only for your convenience and therefore you access them at your own risk. Nonetheless, VEBMUHANDISdemo.com seeks to protect the integrity of its website and the links placed upon it and therefore requests any feedback on not only its own site, but for sites it links to as well (including if a specific link does not work).</p>
+                            <h5>Contacting Us</h5>
+                            <p class="mb-0">If there are any questions regarding this privacy policy you may contact us.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+<!-- /Main -->
+
+<!-- Preloader -->
+<div id="loader-wrapper">
+    <div class="preloader d-flex align-items-center justify-content-center">
+        <div class="loaderPill">
+            <div class="loaderPill-anim">
+                <div class="loaderPill-anim-bounce">
+                    <div class="loaderPill-anim-flop">
+                        <div class="loaderPill-pill"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="loaderPill-floor">
+                <div class="loaderPill-floor-shadow"></div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- /Preloader -->
+
+<!-- jQuery -->
+<script src="assets/js/jquery-3.6.0.min.js"></script>
+
+<!-- Bootstrap Core JS -->
+<script src="assets/js/bootstrap.bundle.min.js"></script>
+
+<!-- Slick JS -->
+<script src="assets/plugins/slick/slick.js"></script>
+
+<!-- Wow JS -->
+<script src="assets/js/wow.js"></script>
+
+<!-- Select2 JS -->
+<script src="assets/plugins/select2/select2.min.js"></script>
+
+<!-- Scrollup JS -->
+<script src="assets/js/scrollup.js"></script>
+
+<!-- Sidebar JS -->
+<script src="assets/plugins/theia-sticky-sidebar/jquery.theia.sticky.js"></script>
+
+<!-- Elevatezoom JS -->
+<script src="assets/js/jquery.elevatezoom.js"></script>
+
+<!-- Scrollbar JS -->
+<script src="assets/plugins/perfect-scrollbar/perfect-scrollbar.js"></script>
+
+<!-- Waypoints JS -->
+<script src="assets/js/waypoints.js"></script>
+
+<!-- Shop JS -->
+<script src="assets/js/shop.js"></script>
+
+<!-- Custom JS -->
+<script src="assets/js/script.js"></script>
+</body>
+</html>
+
