f296a83337ab248821042e09b0c6f7e4318d1bbd
abduraxmon <abduraxmon.jalolov12@gmail.com>
2023-03-20 21:30:05 +0500

    mana

--- app/Http/Livewire/Elonlar/IndexLivewire.php
+++ app/Http/Livewire/Elonlar/IndexLivewire.php
@@ -8,17 +8,23 @@ use Livewire\Component;
 class IndexLivewire extends Component
 {
     public $count=8;
+    public $elon=false;
      protected $listeners=[
         'loadmore'
     ];
+
     public function loadmore(){
        $this->count+=8;
     }
-   
+
+    public function showElon($id){
+        $elon = Announcement::find($id);
+        $this->elon = $elon;
+    }
     public function render()
     {
         $elonlar=Announcement::take($this->count)->get();
-
-        return view('livewire.elonlar.index-livewire',compact('elonlar'));
+        $elon=$this->elon;
+        return view('livewire.elonlar.index-livewire',compact('elonlar','elon'));
     }
 }
--- resources/views/livewire/elonlar/index-livewire.blade.php
+++ resources/views/livewire/elonlar/index-livewire.blade.php
@@ -9,27 +9,29 @@
                         <a href="{{ route('front.view-product') }}">
                             @php
                             if (!count($announcement->images)==0){
-
                             $name=$announcement->images[0]->name;
                             }
                             @endphp
                             <img src="{{asset('images/'.$name)}}" alt="">
                             @php
-
                                 $name='';
-
                             @endphp
                         </a>
                     </div>
                     <div class="product-inner-details">
-                        <a aria-label="Quick view" class="product-btn" data-bs-toggle="modal"
-                            data-bs-target="#quickViewModal"><i class="fi-rs-eye"></i></a>
-                        <a aria-label="Search" class="product-btn" href="#"><i
-                                class="fi-rs-search"></i></a>
+                        <a aria-label="Quick view" class="product-btn"  wire:click="showElon({{$announcement->id}})"  data-bs-toggle="modal" data-bs-target="#quickViewModal">
+                            <i class="fi-rs-eye"></i>
+                        </a>
+                        <a aria-label="Search" class="product-btn" href="#">
+                            <i class="fi-rs-search"></i>
+                        </a>
                         <a aria-label="Add To Wishlist" class="product-btn"
-                            href="{{ route('front.wishlist') }}"><i class="fi-rs-heart"></i></a>
-                        <a href="cart.html" aria-label="Cart" class="product-btn"><i
-                                class="fi-rs-shopping-cart"></i></a>
+                            href="{{ route('front.wishlist') }}"><i class="fi-rs-heart"></i>
+                        </a>
+                        <a href="cart.html" aria-label="Cart" class="product-btn">
+                            <i
+                                class="fi-rs-shopping-cart"></i>
+                        </a>
                     </div>
                     <div class="product-badge">
                         <span class="best">{{ $announcement->type }}</span>
@@ -56,6 +58,150 @@
                     </div>
                 </div>
             </div>
+
+            <!-- Quick view -->
+            <div class="modal fade custom-modal" id="quickViewModal" tabindex="-1" aria-labelledby="quickViewModal" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <button type="button" class="btn-close quick-close" data-bs-dismiss="modal" aria-label="Close">
+                            <i class="feather-x-circle"></i>
+                        </button>
+                        <div class="modal-body">
+                            <div class="row">
+                                <div class="col-md-6 col-sm-12 col-xs-12 mb-md-0 mb-sm-5">
+                                    <div class="detail-gallery">
+                                        <!-- MAIN SLIDES -->
+                                        <div class="position-relative">
+                                            <div class="product-image-slider">
+                                                <figure class="border-radius-10">
+                                                    <img src="assets/img/shop/product-big-1.jpg" alt="product image">
+                                                </figure>
+                                                <figure class="border-radius-10">
+                                                    <img src="assets/img/shop/product-big-2.jpg" alt="product image">
+                                                </figure>
+                                                <figure class="border-radius-10">
+                                                    <img src="assets/img/shop/product-big-1.jpg" alt="product image">
+                                                </figure>
+                                                <figure class="border-radius-10">
+                                                    <img src="assets/img/shop/product-big-2.jpg" alt="product image">
+                                                </figure>
+                                                <figure class="border-radius-10">
+                                                    <img src="assets/img/shop/product-big-1.jpg" alt="product image">
+                                                </figure>
+                                                <figure class="border-radius-10">
+                                                    <img src="assets/img/shop/product-big-2.jpg" alt="product image">
+                                                </figure>
+                                                <figure class="border-radius-10">
+                                                    <img src="assets/img/shop/product-big-1.jpg" alt="product image">
+                                                </figure>
+                                            </div>
+                                            <span class="zoom-icon"><i class="feather-maximize-2"></i></span>
+                                        </div>
+                                        <!-- THUMBNAILS -->
+                                        <div class="slider-nav-thumbnails">
+                                            <div><img src="assets/img/shop/product-big-1.jpg" alt="product image"></div>
+                                            <div><img src="assets/img/shop/product-big-2.jpg" alt="product image"></div>
+                                            <div><img src="assets/img/shop/product-big-1.jpg" alt="product image"></div>
+                                            <div><img src="assets/img/shop/product-big-2.jpg" alt="product image"></div>
+                                            <div><img src="assets/img/shop/product-big-1.jpg" alt="product image"></div>
+                                            <div><img src="assets/img/shop/product-big-2.jpg" alt="product image"></div>
+                                            <div><img src="assets/img/shop/product-big-1.jpg" alt="product image"></div>
+                                        </div>
+                                    </div>
+                                    <!-- End Gallery -->
+                                </div>
+                                <div class="col-md-6 col-sm-12 col-xs-12">
+                                    <div class="detail-info">
+                                        <h5 class="title-detail">EYEBOGLER Regular Fit Men's Cotton Shirt</h5>
+                                        <div class="clearfix product-price-cover">
+                                            <div class="product-price primary-color float-left">
+                                                <span class="current-price">$ 350.00</span>
+                                                <span class="old-price">$ 450.00</span>
+                                                <span class="save-price">-72%</span>
+                                            </div>
+                                        </div>
+                                        <ul class="pro-code">
+                                            <li>Product Code : <span class="text-black">T285HS</span></li>
+                                            <li>Categories : <span class="text-black">Shirt</span></li>
+                                        </ul>
+                                        <div class="rating d-inline-block mb-3">
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star-half-alt"></i>
+                                            <i class="far fa-star"></i>
+                                            <span class="ml-5"> (3.5)</span>
+                                        </div>
+                                        <p class="in-stock text-brand">39 in Stock</p>
+                                        <div class="detail-extralink">
+                                            <div class="detail-qty border radius">
+                                                <a href="#" class="qty-down"><i class="fi-rs-minus-small"></i></a>
+                                                <span class="qty-val">1</span>
+                                                <a href="#" class="qty-up"><i class="fi-rs-plus-small"></i></a>
+                                            </div>
+                                        </div>
+                                        <div class="product-extra-link2">
+                                            <button type="submit" class="button button-add-to-cart me-3"><i
+                                                    class="fi-rs-shopping-cart"></i> ADD TO CART</button>
+                                            <a aria-label="Add To Wishlist" class="button btn-wishlist"
+                                               href="wishlist.html"><i class="fi-rs-heart me-1 ms-1"></i></a>
+                                        </div>
+                                        <div class="pro-share">
+                                            <ul>
+                                                <li class="me-2"><span>Share :</span></li>
+                                                <li><a href="#"><i class="fab fa-facebook"></i></a></li>
+                                                <li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+                                                <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                                                <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <!-- Detail Info -->
+
+                                    <div class="product-info p-0 border-0">
+                                        <div class="tab-style3">
+                                            <ul class="nav nav-tabs text-uppercase modal-nav">
+                                                <li class="nav-item">
+                                                    <a class="nav-link active" id="Description-tab" data-bs-toggle="tab"
+                                                       href="#Description">Description</a>
+                                                </li>
+                                                <li class="nav-item">
+                                                    <a class="nav-link" id="Additional-info-tab" data-bs-toggle="tab"
+                                                       href="#Specification">Specification</a>
+                                                </li>
+                                            </ul>
+                                            <div class="tab-content shop_info_tab entry-main-content">
+                                                <div class="tab-pane fade show active" id="Description">
+                                                    <div class="">
+                                                        <ul class="pro-desc">
+                                                            <li>Product Dimensions : 65 x 46 x 1 cm; 240 Grams
+                                                            <li>Date First Available : 31 October 2020</li>
+                                                            <li>Manufacturer : Seven Rocks International</li>
+                                                            <li>ASIN : B08MCZMNDW</li>
+                                                            <li>Item model number : T285HS</li>
+                                                        </ul>
+                                                    </div>
+                                                </div>
+                                                <div class="tab-pane fade" id="Specification">
+                                                    <ul class="pro-desc">
+                                                        <li>Item model number : T285HS</li>
+                                                        <li>Department : Men</li>
+                                                        <li>Packer : Seven Rocks International</li>
+                                                        <li>Importer : Seven Rocks International</li>
+                                                        <li>Item Weight : 240 g</li>
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- /Quick view -->
+
         </div>
     @endforeach
     <!-- /Product box -->
